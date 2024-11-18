b0ff5dff55fbe29c6c0af325d9dd650774591fce
Oğuzhan <oguzhancevik.developer@gmail.com>
2024-11-18 16:44:33 +0300

    login -register added

--- frontend/src/app/components/auths/login/login.component.ts
+++ frontend/src/app/components/auths/login/login.component.ts
@@ -1,12 +1,57 @@
+import { GoogleSigninButtonModule, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { SharedModule } from '../../../commons/modules/shared.module';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [SharedModule,GoogleSigninButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  
+  user: SocialUser;
+  loggedIn: boolean;
+
+  constructor(
+    private _auth: AuthService,
+    private authService: SocialAuthService
+  ) { }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+      this._auth.googleLogin(this.user);
+    });
+  }
+
+  login(form: NgForm) {
+    if (form.valid) {
+      this._auth.login(form.value);
+    }
+  }
+
+  showOrHidePassword(password: HTMLInputElement) {
+    if (password.type == "password")
+      password.type = "text"
+    else {
+      password.type = "password"
+    }
+  }
+
+  sendConfirmMail(form: NgForm) {
+    if (form.valid)
+      this._auth.sendConfirmMail(form.controls["emailOrUserNameForConfirmEmail"].value);
+  }
+
+  sendForgotPasswordMail(form: NgForm) {
+    if (form.valid) {
+      this._auth.sendForgotPasswordMail(form.controls["emailOrUserNameForForgotPassword"].value);
+    }
+  }
 }
--- frontend/src/app/components/auths/register/register.component.ts
+++ frontend/src/app/components/auths/register/register.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { SharedModule } from '../../../commons/modules/shared.module';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  
+  constructor(
+    private _auth: AuthService
+  ){}
+
+  register(form: NgForm){
+    if(form.valid){
+      this._auth.register(form.value);
+    }
+  }
+
+  showOrHidePassword(password: HTMLInputElement){
+    if(password.type == "password")
+      password.type = "text"
+    else{
+      password.type = "password"
+    }
+  }
 
 }
