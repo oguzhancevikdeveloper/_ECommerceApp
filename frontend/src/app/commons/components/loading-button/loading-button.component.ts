408ec1f685f6e5ad2eddc17fb1f74c449dea93f1
Oğuzhan <oguzhancevik.developer@gmail.com>
2024-11-18 16:35:26 +0300

    loading button and forgot-password added

--- frontend/src/app/commons/components/loading-button/loading-button.component.ts
+++ frontend/src/app/commons/components/loading-button/loading-button.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgForm } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-loading-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './loading-button.component.html',
   styleUrl: './loading-button.component.css'
 })
 export class LoadingButtonComponent {
-
+  @Input() form: NgForm;
+  @Input() btnName: string = "";
+  @Input() btnClassName: string = "btn-outline-primary w-100";
+  @Input() btnLoadingClassName: string = "btn-outline-primary w-100";
+  @Input() btnLoadingName: string = "";
+  @Input() iconClassName:string = "";
+  @Input() iconLoadingClassName: string = ""
+  
+  isLoading$: Observable<boolean>
+  
+  constructor(
+    private store: Store<{isLoading: boolean}>
+  ){
+    this.isLoading$ = store.select("isLoading");
+  } 
 }
